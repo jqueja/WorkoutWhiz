fda6c9eeb615b9b5eb0df6176b2028192477db6e
Josh <jqueja@calpoly.edu>
2023-11-25 15:11:40 -0800

    sign up is working on the backend

--- backend/src/api/server.py
+++ backend/src/api/server.py
@@ -22,10 +22,10 @@ app = FastAPI(
 )
 
 app.include_router(settings.router)
-app.include_router(login.router)
-app.include_router(signup.router)
-app.include_router(info.router)
-app.include_router(workouts.router)
+#app.include_router(login.router)
+#app.include_router(signup.router)
+#app.include_router(info.router)
+#app.include_router(workouts.router)
 
 
 @app.exception_handler(exceptions.RequestValidationError)
--- backend/src/api/signup.py
+++ backend/src/api/signup.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter, Request, HTTPException, status
-from pydantic import BaseModel
-import sqlalchemy
-from src import database as db
+from dotenv import load_dotenv
+load_dotenv()
 
+import os
 from supabase import create_client
 
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
+
+user_email: str = 'quejajosh01@gmail.com'
+user_password: str = 'LiptonGreenTea202!'
+
+new_user = supabase.auth.sign_up({ "email": user_email, "password": user_password })
+
+user_id = new_user.user.id
+print(f"User ID: {user_id}")
