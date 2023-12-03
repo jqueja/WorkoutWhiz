f394bf5161d4d66811fa241843be9ef173b4cec6
Hayley <haysho2260@gmail.com>
2023-12-02 20:11:01 -0800

    check whether data is logged in test

--- cypress/cypress/e2e/Login.cy.js
+++ cypress/cypress/e2e/Login.cy.js
@@ -1,54 +1,26 @@
-// cypress/integration/login_spec.js
+describe("The Login page", () => {
+     it("successfully loads", () => {
+          cy.visit("/login");
 
-describe("Login Page", () => {
-     beforeEach(() => {
-          cy.visit("http://localhost:3000/login"); // Update the URL accordingly
-     });
-
-     it("should successfully login with valid credentials", () => {
-          // Input valid credentials and click the login button
-          cy.get("#typeEmailX").type("jchou18@calpoly.edu");
-          cy.get("#typePasswordX").type("njAiHP!y#VcY5#");
-          cy.get("button").contains("Login").click();
-
-          // Assert that navigation occurred after successful login
-          cy.url().should("eq", "http://localhost:3000/");
-
-          // Check if the home page is displayed
-          cy.get(".page-header").should("contain", "My Workout Log");
-
-          // Check if
-          cy.get(
-               'button.btn.btn-primary[style="height: 3rem; width: 3rem; padding: 0rem; background: rgb(246, 88, 88); border-color: rgb(246, 88, 88);"]'
-          ).should("exist");
-          // Open LogWorkout
-          cy.get(
-               'button.btn.btn-primary[style="height: 3rem; width: 3rem; padding: 0rem; background: rgb(246, 88, 88); border-color: rgb(246, 88, 88);"]'
-          ).click();
-
-          // Verify the form elements
-          cy.get('input[name="date"]').should("exist");
-          cy.get('input[name="workoutName"]').should("exist");
-          cy.get('input[name="weight"]').should("exist");
-          cy.get('input[name="sets"]').should("exist");
-          cy.get('input[name="reps"]').should("exist");
-          cy.get('button[type="submit"]').should("exist");
-
-          // Fill in the workout details
-          cy.get('input[name="date"]').type("2023-01-01");
-          cy.get('input[name="workoutName"]').type("Bench Press");
-          cy.get('input[name="weight"]').type("100");
-          cy.get('input[name="sets"]').type("3");
-          cy.get('input[name="reps"]').type("10");
-
-          // Check if the workout is successfully logged
-          cy.get(".alert").should("not.exist"); // Check if there are no error alerts
-
-          // Verify the close button
-          cy.get('button.btn-close[aria-label="Close"]').should("exist");
-          cy.get('button.btn-close[aria-label="Close"]').click();
-
-          // Check if the new workout card is displayed
-          cy.get(".WorkoutCard").should("exist");
+          cy.get(".card-body.text-center").should("exist");
+          cy.get("h2.page-header").should("contain", "Login");
+          cy.get("p.text-black-50").should(
+               "contain",
+               "Please enter your login and password."
+          );
+          cy.get('div.form-outline.form-white input[type="email"]').should(
+               "exist"
+          );
+          cy.get('div.form-outline.form-white input[type="password"]').should(
+               "exist"
+          );
+          cy.get("a.text-black-50").should("contain", "Forgot password?");
+          cy.get("button.btn.btn-primary").should("exist");
+          cy.get("p.mb-1").should("contain", "Don't have an account?");
+          cy.get("a.text-black-50.fw-bold").should(
+               "have.attr",
+               "href",
+               "/signup"
+          );
      });
 });
---
+++ cypress/cypress/e2e/Login_to_Log.cy.js
@@ -0,0 +1,80 @@
+// cypress/integration/login_spec.js
+
+describe("Login Page", () => {
+     beforeEach(() => {
+          cy.visit("http://localhost:3000/login"); // Update the URL accordingly
+     });
+
+     it("should successfully login with valid credentials", () => {
+          // Input valid credentials and click the login button
+          cy.get("#typeEmailX").type("jchou18@calpoly.edu");
+          cy.get("#typePasswordX").type("njAiHP!y#VcY5#");
+          cy.get("button").contains("Login").click();
+
+          // Assert that navigation occurred after successful login
+          cy.url().should("eq", "http://localhost:3000/");
+
+          // Check if the home page is displayed
+          cy.get(".page-header").should("contain", "My Workout Log");
+
+          // Check if
+          cy.get(
+               'button.btn.btn-primary[style="height: 3rem; width: 3rem; padding: 0rem; background: rgb(246, 88, 88); border-color: rgb(246, 88, 88);"]'
+          ).should("exist");
+
+          // Open LogWorkout
+          cy.get(
+               'button.btn.btn-primary[style="height: 3rem; width: 3rem; padding: 0rem; background: rgb(246, 88, 88); border-color: rgb(246, 88, 88);"]'
+          ).click();
+
+          // Verify the form elements
+          cy.get('input[name="date"]').should("exist");
+          cy.get('input[name="workoutName"]').should("exist");
+          cy.get('input[name="weight"]').should("exist");
+          cy.get('input[name="sets"]').should("exist");
+          cy.get('input[name="reps"]').should("exist");
+          cy.get('button[type="submit"]').should("exist");
+
+          // Fill in the workout details
+          cy.get('input[name="date"]').type("2023-01-01");
+          cy.get('input[name="workoutName"]').clear().type("Bench Press");
+          cy.get('input[name="weight"]').clear().type(Number("100"));
+          cy.get('input[name="sets"]').clear().type(Number("3"));
+          cy.get('input[name="reps"]').clear().type(Number("10"));
+
+          // Check if the workout is successfully logged
+          cy.get(".alert").should("not.exist"); // Check if there are no error alerts
+
+          // Verify the close button
+          cy.get('button.btn-close[aria-label="Close"]').should("exist");
+          cy.get('button.btn-close[aria-label="Close"]').click();
+
+          // Check if the new workout card is displayed
+          cy.get(".card").should("exist");
+
+          // Select the specific card based on its content
+          cy.contains(".card-title.h5", "2023-01-01")
+               .parents(".card")
+               .within(() => {
+                    // Verify the card content
+                    cy.get(".table th").should("have.length", 4);
+                    cy.get(".table th").eq(0).should("have.text", "Exercise");
+                    cy.get(".table th").eq(1).should("have.text", "Weight");
+                    cy.get(".table th").eq(2).should("have.text", "Sets");
+                    cy.get(".table th").eq(3).should("have.text", "Reps");
+
+                    // Verify table row content
+                    cy.get(".table tbody tr").should("have.length", 1);
+                    cy.get(".table tbody tr td")
+                         .eq(0)
+                         .should("have.text", "Bench Press");
+                    cy.get(".table tbody tr td")
+                         .eq(1)
+                         .should("have.text", "100");
+                    cy.get(".table tbody tr td").eq(2).should("have.text", "3");
+                    cy.get(".table tbody tr td")
+                         .eq(3)
+                         .should("have.text", "10");
+               });
+     });
+});
