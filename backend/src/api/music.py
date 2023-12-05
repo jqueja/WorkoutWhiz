d6bd1225c5564b50b38a16444016850cbb94effa
Josh Queja <jqueja@calpoly.edu>
2023-12-04 18:24:51 -0800

    added the check of duplication in the database

--- backend/src/api/music.py
+++ backend/src/api/music.py
@@ -30,11 +30,28 @@ class MusicRequest(BaseModel):
 @router.post("/add-music")
 def add_music(request: MusicRequest):
 
+    # Checks for duplications in the database
+    with db.engine.begin() as connection:
+        record = connection.execute(
+            sqlalchemy.text(
+            """
+            SELECT *
+            FROM music_links
+            WHERE "user" = :user_id AND "link" = :user_link
+            """),
+            {"user_id": request.user_id, "user_link": request.link}
+        )
+
+    existing_record = record.fetchone()
+
+    if existing_record:
+        raise HTTPException(status_code=409, detail="Song is already associated with user")
+
     with db.engine.begin() as connection:
         result = connection.execute(
             sqlalchemy.text(
             """
-            INSERT INTO music_links (user, link)
+            INSERT INTO music_links ("user", "link")
             VALUES (:user_id, :user_link)
             """),
             {"user_id": request.user_id, "user_link": request.link}
